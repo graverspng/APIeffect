8dc45f3fe22eed889442f2f0fc80bda8d69103b5
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-31 09:53:59 +0200

    heh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ToDo from './ToDo.js'
 
 function App() {
 
-  const allChecks = {
-    userID: 1,
-    id: 1,
-    title: "delicious aut autem",
-    completed: false,
-  };
+  const [toDo, setToDo] = useState({});
+    
+  useEffect(() => {
+    async function getToDo() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await response.json()
+    setToDo(data);
+  }
+  
+    getToDo();
+  },[]);
+  
 
 
   return (
     <>
-    <ToDo {...allChecks}/>
+    <ToDo{...toDo} />
     </>
   );
 }
